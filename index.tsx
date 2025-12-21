135992e2b123776d9e6bed31f2a07a4c0ee9cc9f
Oxsilaris06 <nicolas.maheux45@gmail.com>
2025-12-21 22:33:18 +0100

    Update index.tsx

--- index.tsx
+++ index.tsx
@@ -1,10 +1,16 @@
-import 'react-native-get-random-values'; // DOIT ÊTRE EN PREMIER
+// 1. D'ABORD : Les polyfills pour les valeurs aléatoires (Vital pour PeerJS)
+import 'react-native-get-random-values';
+
+// 2. ENSUITE : Expo
 import { registerRootComponent } from 'expo';
+
+// 3. PUIS : Le Polyfill WebRTC
 import { registerGlobals } from 'react-native-webrtc';
 
-// Activation WebRTC
+// 4. ACTIVATION
 registerGlobals();
 
+// 5. ENFIN : L'Application
 import App from './App';
 
 registerRootComponent(App);
