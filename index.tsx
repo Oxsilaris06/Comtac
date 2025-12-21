ba2d372c98e6303f31e0ad0c5be3099112c58f5d
Oxsilaris06 <nicolas.maheux45@gmail.com>
2025-12-21 22:27:45 +0100

    Update index.tsx

--- index.tsx
+++ index.tsx
@@ -1,16 +1,10 @@
-// 1. D'ABORD ET AVANT TOUT : Les polyfills Node.js
-import 'react-native-get-random-values';
-
-// 2. ENSUITE : Expo
+import 'react-native-get-random-values'; // DOIT ÊTRE EN PREMIER
 import { registerRootComponent } from 'expo';
-
-// 3. PUIS : WebRTC
 import { registerGlobals } from 'react-native-webrtc';
 
-// 4. ACTIVATION
+// Activation WebRTC
 registerGlobals();
 
-// 5. ENFIN : L'App
 import App from './App';
 
 registerRootComponent(App);
