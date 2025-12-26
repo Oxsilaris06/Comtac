1be206af2f2e09215644720f586ed299eba78f07
Oxsilaris06 <nicolas.maheux45@gmail.com>
2025-12-26 16:10:55 +0100

    Update types.ts

--- types.ts
+++ types.ts
@@ -11,7 +11,7 @@ export enum OperatorStatus {
   PROGRESSION = 'PROGRESSION',
 }
 
-export type ViewType = 'login' | 'menu' | 'ops' | 'map';
+export type ViewType = 'login' | 'menu' | 'ops' | 'map' | 'settings'; // Ajout de 'settings'
 
 export interface UserData {
   id: string;
@@ -34,3 +34,20 @@ export interface PingData {
   sender: string;
   timestamp: number;
 }
+
+// --- NOUVEAU : CONFIGURATION ---
+export interface AppSettings {
+  audioOutput: 'defaut' | 'casque' | 'hp'; // Force la sortie
+  gpsUpdateInterval: number; // en ms (ex: 1000, 5000, 10000)
+  pttKey: number; // KeyCode du bouton PTT physique (ex: 24 pour Vol UP)
+  userArrowColor: string; // Couleur perso sur la carte
+  theme: 'dark' | 'light'; // Pour le futur
+}
+
+export const DEFAULT_SETTINGS: AppSettings = {
+  audioOutput: 'defaut',
+  gpsUpdateInterval: 5000,
+  pttKey: 24, // VOLUME UP par défaut
+  userArrowColor: '#3b82f6', // Bleu par défaut
+  theme: 'dark'
+};
